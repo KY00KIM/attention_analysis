563d7481d28f529debd962f151927f117b8bd0e8
KY00KIM <dev.kyoomin@gmail.com>
2024-03-23 15:06:11 +0800

    Add profile sdpa flash_attention

--- hf_transformer/main.py
+++ hf_transformer/main.py
@@ -1,30 +1,59 @@
-from transformers import AutoTokenizer
 import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from torch.profiler import profile, record_function, ProfilerActivity
+import argparse
+ 
+def load_model(attention: str = "sdpa"):
+    model_id = "meta-llama/Llama-2-7b-hf"
 
-model = "meta-llama/Llama-2-7b-hf"
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-tokenizer = AutoTokenizer.from_pretrained(model)
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    torch_dtype=torch.float16,
-    device_map="auto",
-)
+    model = AutoModelForCausalLM.from_pretrained(
+    "meta-llama/Llama-2-7b-hf", 
+    torch_dtype=torch.bfloat16, 
+    attn_implementation=attention,
+    )
 
-sequences = pipeline(
-    'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n',
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-    max_length=200,
-)
-for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        torch_dtype=torch.float16,
+        device="cuda",
+        tokenizer=tokenizer,
+    )
+    return (pipeline, tokenizer)
 
-def run():
-    print("Run!!")
+def inference(input_query, pipeline, tokenizer):
+    sequences = pipeline(
+        input_query,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+    )
+    print("########### Result #############")
+    for seq in sequences:
+        print(f"Result: {seq['generated_text']}")
+    print("###############################")
+
+def run(attention: str = "sdpa"):
+    input_query = 'Hi I\'m a CS major college student. Introduce yourself.\n'
+
+    pipeline, tokenizer = load_model(attention=attention)
+
+    with profile(activities=[
+        ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+        with record_function("model_inference"):
+           inference(input_query, pipeline, tokenizer)
+
+    print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=200))
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--attention", type=str, default="sdpa", choices=["sdpa", "flash_attention_2"], help="Attention implementation")
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    print("Hello World")
+    args = parse_args()
+    run(attention=args.attention)
